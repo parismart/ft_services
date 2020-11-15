c0110d032dadbc68edc3f75204756a6f43a58879
¨parmarti¨ <parismrt@gmail.com>
2020-11-15 22:04:22 +0100

    comments

--- setup.sh
+++ setup.sh
@@ -16,6 +16,10 @@ minikube start --driver=virtualbox
 eval $(minikube docker-env)
 
 # Build docker images.
+# ""> /dev/null 2>&1" redirects the output of your program to /dev/null. Include both the Standard Error and Standard Out
+# ">" is for redirect. "/dev/null" is a black hole where any data sent, will be discarded
+# "2" is the file descriptor for Standard Error. "1" is the file descriptor for Standard Out.
+# "&" is the symbol for file descriptor (without it, the following 1 would be considered a filename).
 echo "${GREEN}Docker build init${END}"
 docker build -t my_nginx srcs/nginx > /dev/null 2>&1
 docker build -t my_wordpress srcs/wordpress > /dev/null 2>&1
--- srcs/nginx/srcs/start.sh
+++ srcs/nginx/srcs/start.sh
@@ -4,4 +4,6 @@
 # sshd (OpennSSH Daemon) is the daemon program for ssh. Together these programs replace rlogin and rsh,
 # and provide secure encrypted communications between two untrusted hosts over an insecure network.
 # For normal production on a server, use the default daemon on. In this case for Docker containers, set the daemon off.
+# "&" run multiple commands simultaneously.
+# "&&" does not allow to continue the execution sequence of the commands, until they are executed consecutively(one by one), correctly
 telegraf & /usr/sbin/sshd && nginx -g 'daemon off;'
