e7c8e59b2e333a3a8603d1bb2e1ab0faf95231f5
parismart <parismart@gmail.com>
2020-12-15 18:19:13 +0100

    range ip for VM

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ minikube delete
 killall -TERM kubectl minikube VBoxHeadless
 
 # Start minikube.
-minikube start --driver=virtualbox
+minikube start --driver=docker
 
 # Use the docker daemon from minikube.
 eval $(minikube docker-env)
@@ -20,7 +20,7 @@ eval $(minikube docker-env)
 # ">" is for redirect. "/dev/null" is a black hole where any data sent, will be discarded
 # "2" is the file descriptor for Standard Error. "1" is the file descriptor for Standard Out.
 # "&" is the symbol for file descriptor (without it, the following 1 would be considered a filename).
-echo -e "${GREEN}Docker build init${END}"
+echo "${GREEN}Docker build init${END}"
 docker build -t my_nginx srcs/nginx > /dev/null 2>&1
 docker build -t my_wordpress srcs/wordpress > /dev/null 2>&1
 docker build -t my_mysql srcs/mysql > /dev/null 2>&1
@@ -28,10 +28,10 @@ docker build -t my_phpmyadmin srcs/phpmyadmin > /dev/null 2>&1
 docker build -t my_ftps srcs/ftps > /dev/null 2>&1
 docker build -t my_grafana srcs/grafana > /dev/null 2>&1
 docker build -t my_influxdb srcs/influxdb > /dev/null 2>&1
-echo -e "${BLUE}Docker build completed${END}"
+echo "${BLUE}Docker build completed${END}"
 
 # Apply yaml resources.
-echo -e "${GREEN}Deploy init${END}"
+echo "${GREEN}Deploy init${END}"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl apply -f srcs/metalLB.yaml
@@ -42,7 +42,7 @@ kubectl apply -f srcs/phpmyadmin.yaml
 kubectl apply -f srcs/wordpress.yaml
 kubectl apply -f srcs/grafana.yaml
 kubectl apply -f srcs/influxdb.yaml
-echo -e "${BLUE}Deploy completed${END}"
+echo "${BLUE}Deploy completed${END}"
 
 # Setup metalLB secret.
 kubectl create secret generic -n metallb-system memberlist  --from-literal=secretkey="$(openssl rand -base64 128)"
